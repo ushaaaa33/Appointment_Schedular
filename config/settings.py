63c7ec17656d2178272af7c866a2977707c86e26
Mandip <mandipadhikari.codes@gmail.com>
2026-03-26 22:05:25 +0545

    feat: implement review model and enhance doctor profile with star rating UI

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'apps.accounts.apps.AccountsConfig',
     'apps.appointments.apps.AppointmentsConfig',
     'apps.services.apps.ServicesConfig',
+    'reviews.apps.ReviewsConfig',
 
 ]
 
